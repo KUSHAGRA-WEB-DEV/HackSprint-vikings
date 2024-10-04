25f7d2f03f68c334420ab68e66f97042f4fef968
KUSHAGRA-WEB-DEV <skushagra589@gmail.com>
2024-10-04 02:19:31 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import time
+from datetime import datetime
+class DigitalClock:
+    def_init(self,master)
+    self.master=master
+    master.title("Digital Clock")
+    self.is_24_hour=True
+    self.label
+             
