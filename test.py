688e05ef1de6899702144ccb108ec289dc574830
KUSHAGRA-WEB-DEV <skushagra589@gmail.com>
2024-10-04 02:39:27 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ import time
 from datetime import datetime
 from typing import Self
 class DigitalClock:
-    def_init (self,master);
+    def_init(Self,master)
     SelfReg.master=master;
     aiter.title("Digital Clock")
     Self.is_24_hour=True;
     Self.label_60_minutes=True;
     Self.label_60_seconds=True;
     Self.update_clock=True;
+    SelfReg.works_clock=True;
+
